6cb5c726b1e6015c46d8634cfe286b5966e6f5d4
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-28 18:28:05 +0530

    err handling

--- backend/auth/user.js
+++ backend/auth/user.js
@@ -9,16 +9,6 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 
 function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch((error) => {
-      document.getElementById("error").innerHTML = error.message;
-      document.getElementById("error2").innerHTML = error.message;
-    });
-
   // ----captcha.js----
 
   document
@@ -29,9 +19,19 @@ function login() {
       if (!recaptchaResponse) {
         event.preventDefault();
         alert("Please complete the reCAPTCHA.");
+        document.getElementById("error2").innerHTML = "Please click i'm not a robot"";
       } else {
         //need to code
       }
+    });
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      document.getElementById("error").innerHTML = error.message;
+      document.getElementById("error2").innerHTML = error.message;
     });
 }
 
