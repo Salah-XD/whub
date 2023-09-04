dfa8be64ded93671c0d41f770b772e26b2b19d76
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-09-05 05:01:35 +0530

    profile

--- backend/auth/welcome.js
+++ backend/auth/welcome.js
@@ -1,8 +1,12 @@
 firebase.auth().onAuthStateChanged((user) => {
   if (!user) {
-    // location.replace("index.html");
+    // location.replace("index.html"); // Redirect unauthorized users
   } else {
-    document.getElementById("user").innerHTML = "Hello, " + user.displayName;
+    const profile = user.displayName;
+    const userProfilePicture = user.photoURL;
+
+    document.getElementById("user").innerHTML = "Konichiwa, " + profile;
+    document.getElementById("pfp").src = userProfilePicture;
     document.getElementById("userEmail").innerHTML = user.email;
   }
 });
